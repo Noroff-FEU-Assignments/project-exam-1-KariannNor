03dbed4ff26893215437dd5f447222ee1d3bdba8
KariannNor <kariann.norheim@gmail.com>
2023-08-08 14:17:26 -0400

    loader updated

--- js/blog-posts.js
+++ js/blog-posts.js
@@ -38,3 +38,15 @@ async function displayBlogPosts() {
 displayBlogPosts();
 
 
+//* loader
+window.addEventListener('load', function() {
+  document.querySelector('.loader').style.display = 'flex';
+});
+
+fetch('https://knorheim.no/roadtrippin-api/wp-json/wp/v2/posts?_embed')
+  .then(response => {
+    
+    document.querySelector('.loader').style.display = 'none';
+
+    return response.json();
+  });
--- js/rest-api.js
+++ js/rest-api.js
@@ -24,35 +24,4 @@ export async function fetchBlogPosts(startIndex = 0, numPosts = -1) {
   }
 }
 
-//* loader
-// window.addEventListener('load', function() {
-//   document.querySelector('.loader').style.display = 'flex';
-// });
 
-// fetch(apiUrl)
-//   .then(response => {
-    
-//     document.querySelector('.loader').style.display = 'none';
-
-//     return response.json();
-//   });
-
-
-  window.addEventListener('load', function() {
-    console.log('Page has finished loading');
-    document.querySelector('.loader').style.display = 'flex';
-  });
-  
-  fetch(apiUrl)
-    .then(response => {
-      console.log('API response received');
-      document.querySelector('.loader').style.display = 'none';
-      return response.json();
-    })
-    .then(data => {
-      console.log('API data:', data);
-      
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
