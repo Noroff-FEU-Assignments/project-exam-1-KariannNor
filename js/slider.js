51e600371a7f7cde27d5cff1ff43508223fb31b4
KariannNor <kariann.norheim@gmail.com>
2023-08-17 13:08:02 -0400

    Adjusted content priority

--- js/slider.js
+++ js/slider.js
@@ -40,43 +40,48 @@ async function fetchAndDisplayBlogPosts(page) {
 
   try {
     const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Error fetching blog posts');
+    }
     const data = await response.json();
 
-    const slider = document.getElementById('slider');
-    slider.innerHTML = ''; 
+    if (Array.isArray(data)) {
+      const slider = document.getElementById('slider');
+      slider.innerHTML = '';
 
-    data.forEach(post => {
-      const thumbnail = document.createElement('div');
-      thumbnail.classList.add('thumbnail');
+      data.forEach(post => {
+        const thumbnail = document.createElement('div');
+        thumbnail.classList.add('thumbnail');
 
-      const thumbnailImg = document.createElement('img');
-      thumbnailImg.classList.add('img-thumb');
-      thumbnailImg.src = post._embedded['wp:featuredmedia'][0].source_url;
-      thumbnailImg.alt = post.title.rendered;
+        const thumbnailImg = document.createElement('img');
+        thumbnailImg.classList.add('img-thumb');
+        thumbnailImg.src = post._embedded['wp:featuredmedia'][0].source_url;
+        thumbnailImg.alt = post.title.rendered;
 
-      thumbnail.appendChild(thumbnailImg);
+        thumbnail.appendChild(thumbnailImg);
 
-      const thumbnailLink = document.createElement('a');
-      thumbnailLink.href = `article.html?post_id=${post.id}`;
+        const thumbnailLink = document.createElement('a');
+        thumbnailLink.href = `article.html?post_id=${post.id}`;
 
-      const thumbnailCaption = document.createElement('p');
-      thumbnailCaption.classList.add('title-thumb');
-      thumbnailCaption.textContent = post.title.rendered;
+        const thumbnailCaption = document.createElement('p');
+        thumbnailCaption.classList.add('title-thumb');
+        thumbnailCaption.textContent = post.title.rendered;
 
-      thumbnailLink.appendChild(thumbnailCaption);
-      thumbnail.appendChild(thumbnailLink);
+        thumbnailLink.appendChild(thumbnailCaption);
+        thumbnail.appendChild(thumbnailLink);
 
-      thumbnail.addEventListener('click', () => goToSlide(thumbnail));
+        thumbnail.addEventListener('click', () => goToSlide(thumbnail));
 
-      thumbnailCaption.addEventListener('click', () => {
-        const titleThumbnail = thumbnail; 
-        goToSlide(titleThumbnail);
+        thumbnailCaption.addEventListener('click', () => {
+          const titleThumbnail = thumbnail;
+          goToSlide(titleThumbnail);
+        });
+
+        slider.appendChild(thumbnail);
       });
-      
-
-      slider.appendChild(thumbnail);
-    });
-
+    } else {
+      console.error('Invalid data format:', data);
+    }
   } catch (error) {
     console.error('Error fetching blog posts:', error);
   }
